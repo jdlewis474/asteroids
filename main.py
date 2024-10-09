1f4a6a820f58c6748850ecac7066b2c0b607a6e3
jdlewis474 <jdlewis0474@gmail.com>
2024-10-09 13:35:08 -0400

    Added frame limits

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ from constants import *
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    game_clock = pygame.time.Clock()
+    dt = 0
     i=0
     while i < 1:
         for event in pygame.event.get():
@@ -11,6 +13,7 @@ def main():
                 return
         screen.fill((0,0,0))
         pygame.display.flip()
+        dt = game_clock.tick(60)
 
 if __name__ == "__main__":
     main()
