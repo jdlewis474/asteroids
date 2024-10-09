1d734f8100054f878f5c632de9365a7b71572f8c
jdlewis474 <jdlewis0474@gmail.com>
2024-10-09 13:27:47 -0400

    Main file draws blank screen. Close button works.

---
+++ main.py
@@ -0,0 +1,16 @@
+import pygame
+from constants import *
+
+def main():
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    i=0
+    while i < 1:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        screen.fill((0,0,0))
+        pygame.display.flip()
+
+if __name__ == "__main__":
+    main()
---
+++ readme.md
@@ -0,0 +1,1 @@
+This is a repository for the game Asteroids, as part of a learning experince on Boot.dev.
